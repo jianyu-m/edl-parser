ed050cef368399633b1fa5c8fa711c97ebae9f4c
jianyu-m <jianyu@hku.hk>
2023-03-30 23:36:00 +0800

    edl fin

--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,14 @@ extern crate pest;
 extern crate pest_derive;
 
 use pest::Parser;
+use std::fs;
 
 #[derive(Parser)]
 #[grammar = "csv.pest"]
 pub struct CSVParser;
 fn main() {
-    let successful_parse = CSVParser::parse(Rule::edl, "{trusted{public int hello();}}");
+    let edls = fs::read_to_string("test.edl")
+        .expect("Should have been able to read the file");
+    let successful_parse = CSVParser::parse(Rule::edl, &edls);
     println!("{:?}", successful_parse);
 }
